2696a99f114985bf6c71decb9fd1892080abbbfb
ASNProject <asnproject02@gmail.com>
2022-03-27 07:30:03 +0700

    Website Teluk Tering

---
+++ base.php
@@ -0,0 +1,103 @@
+<?php 
+include_once('pindah_masuk_mail.php');
+?>
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Permohonan Pindah Keluar</title>
+    <link rel="stylesheet" href="style.css" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600&display=swap"
+      rel="stylesheet"
+    />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+      crossorigin="anonymous"
+    />
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+      crossorigin="anonymous"
+    ></script>
+  </head>
+  <body>
+    <div class="wrapper">
+      <nav>
+        <div class="container-flex">
+          <div class="brand">
+            <img
+              src="images/logo.png"
+              alt=""
+              style="width: 60px; height: 80px"
+            />
+          </div>
+          <div class="burger">
+            <div class="bar1"></div>
+            <div class="bar2"></div>
+            <div class="bar3"></div>
+          </div>
+          <div class="bg-sidebar"></div>
+          <ul class="sidebar">
+            <li><a href="#BERANDA">BERANDA</a></li>
+            <li><a href="#service">PROFIL</a></li>
+            <li><a href="#contact">PENGURUSAN DOKUMEN</a></li>
+            <li><a href="#blog">INFORMASI DATA</a></li>
+            <li><a href="#login">PENGADUAN LAYANAN</a></li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+    <!--Main-->
+
+    <!--Bottom-->
+    <div
+      class="container"
+      style="margin-top: 10px; background-color: rgb(212, 208, 208)"
+    >
+      <div class="row" >
+        <div class="col-6" style="padding: 20px;">
+          <p style="margin-left: auto">
+            <img
+              src="images/gmail.png"
+              style="float: left; width: 20px; height: 20px"
+            />telukkering003@gmail.com
+          </p>
+          <p style="margin-left: auto">
+            <img
+              src="images/fb.png"
+              style="float: left; width: 20px; height: 20px"
+            />kelurahan teluk kering
+          </p>
+          <p style="margin-left: auto">
+            <img
+              src="images/wa.png"
+              style="float: left; width: 20px; height: 20px"
+            />085891674893
+          </p>
+        </div>
+        <div class="col-6" style=" margin-right: auto;">
+          <div>
+            <p style="                
+                display: block;
+                margin-left: auto;
+                margin-right: auto;
+                width: 50%;
+                margin-top: 10px;"
+                >LOKASI KELURAHAN TELUK KERING</p>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31912.368026676064!2d104.02729576060487!3d1.1273684473697556!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31d9891afdb3cd87%3A0xbc627a0dc73e7499!2sTlk.%20Tering%2C%20Kec.%20Batam%20Kota%2C%20Kota%20Batam%2C%20Kepulauan%20Riau!5e0!3m2!1sid!2sid!4v1647777580152!5m2!1sid!2sid" width="520" height="200" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <p style="margin-left: auto; font-size: 10px; margin-top: 10px;">
+        Copyright&copy; Teluk Kering Online | 2022
+      </p>
+    </div>
+  </body>
+</html>
---
+++ pindah_keluar_mail.php
@@ -0,0 +1,72 @@
+<?php
+$message = '';
+
+if (isset($_POST["submit"])){
+    $path = 'upload/' . $_FILES["pengantar"]["name"];
+    move_uploaded_file($_FILES["pengantar"]["tmp_name"], $path);
+    $path2 = 'upload/' . $_FILES["ktp"]["name"];
+    move_uploaded_file($_FILES["ktp"]["tmp_name"], $path2);
+    $path3 = 'upload/' . $_FILES["kk"]["name"];
+    move_uploaded_file($_FILES["kk"]["tmp_name"], $path3);
+    $message = '
+    <h3 align="center">Permohonan Pindah Keluar</h3>
+    <table border="1" width="100%" cellpadding="5" cellspacing="5">
+        <tr>
+            <td width="30%">Nama</td>
+            <td width="70%">' .$_POST["nama"] . '</td>
+        </tr>
+        <tr>
+            <td width="30%">NIK</td>
+            <td width="70%">' .$_POST["nik"] . '</td>
+        </tr>
+        <tr>
+            <td width="30%">Email</td>
+            <td width="70%">' .$_POST["email"] . '</td>
+        </tr>
+        <tr>
+            <td width="30%">No.HP</td>
+            <td width="70%">' .$_POST["nohp"] . '</td>
+        </tr>
+        <tr>
+            <td width="30%">Alamat Domisili</td>
+            <td width="70%">' .$_POST["alamatdomisili"] . '</td>
+        </tr>
+        <tr>
+            <td width="30%">Alamat Tujuan</td>
+            <td width="70%">' .$_POST["alamattujuan"] . '</td>
+        </tr>
+    ';
+
+require 'class/class.phpmailer.php';
+$mail = new PHPMailer;
+$mail->IsSMTP();
+
+// ADD YOUR DETAILS HERE
+    $mail->Host = 'smtp.gmail.com';        //Sets the SMTP hosts of your Email hosting, this for Godaddy
+    $mail->Port = '465';                                //Sets the default SMTP server port
+    $mail->SMTPAuth = true;                            //Sets SMTP authentication. Utilizes the Username and Password variables
+    $mail->Username = 'telukkeringsend01@gmail.com';                    //Sets SMTP username
+    $mail->Password = 'admintelukkering123';                    //Sets SMTP password
+    $mail->SMTPSecure = 'ssl';                            //Sets connection prefix. Options are "", "ssl" or "tls"
+    $mail->From = 'telukkeringsend01@gmail.com';                      //Sets the From email address for the message
+    $mail->FromName = '' .$_POST["nama"] . '';                //Sets the From name of the message
+    $mail->AddAddress('telukkering01@gmail.com', 'admin telukkering');        //Adds a "To" address
+// ADD YOUR DETAILS HERE
+    
+    $mail->WordWrap = 50;                            //Sets word wrapping on the body of the message to a given number of characters
+    $mail->IsHTML(true);                            //Sets message type to HTML
+    $mail->AddAttachment($path);
+    $mail->AddAttachment($path2);
+    $mail->AddAttachment($path3);//Adds an attachment from a path on the filesystem
+    $mail->Subject = 'Permohonan Pindah Keluar';                //Sets the Subject of the message
+    $mail->Body = $message;                            //An HTML or plain text message body
+    if ($mail->Send())                                //Send an Email. Return true on success or false on error
+    {
+        $message = '<div class="alert alert-success">Application Successfully Submitted</div>';
+        unlink($path);
+    } else {
+        $message = '<div class="alert alert-danger">There is an Error</div>';
+    }
+}
+
+?>
